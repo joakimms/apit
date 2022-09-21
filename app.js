57f119f17d036d6cd9b08036008f976d98487682
Joakim Soderberg <joakim.soderberg@aalto.fi>
2022-09-21 16:43:15 +0300

    initial commmit

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 const handleRequest = (request) => {
-  return new Response("Hello world!");
+  const url = new URL(request.url);
+  const params = url.searchParams;
+
+  const one = Number(params.get("one"));
+  const two = Number(params.get("two"));
+
+  return new Response(`Sum: ${one + two}`);
 };
 
-serve(handleRequest, { port: 7777 });
+let port = 7777;
+if (Deno.args.length > 0) {
+  const lastArgument = Deno.args[Deno.args.length - 1];
+  port = Number(lastArgument);
+}
+
+serve(handleRequest, { port: port });
